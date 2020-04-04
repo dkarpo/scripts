0ce5a83e99e92453fb20980656b24c2e0ac8f8d7
Derrick Karpo <dkarpo@gmail.com>
2020-04-04 15:00:02 -0600

    Sync local version with Github.

--- arduino/usb-keyboard-keepalive/usb-keyboard-keepalive.ino
+++ arduino/usb-keyboard-keepalive/usb-keyboard-keepalive.ino
@@ -1,13 +1,13 @@
 /*
   USB Keyboard emulation intended to act as a "keepalive" to prevent
-  devices from sleeping.  This is useful to keep Android phones (with
-  an OTG cable) and computer systems awake if needed.  This script
+  devices from sleeping. This is useful to keep Android phones (with
+  an OTG cable) and computer systems awake if needed. This script
   requires an ATMega 32u4 based chip as those devices have native USB
-  HID built in.  A great  device for this is the CJMCU Beetle which
+  HID built in. A great  device for this is the CJMCU Beetle which
   has a form factor that is half the size of a regular USB key and has
   a ATMega 32u4 chip on it.
 
-  Sample keyboard codes can be found at: 
+  Sample keyboard codes can be found at:
     http://www.arduino.cc/en/Reference/KeyboardModifiers
 
   Note: Do not set the keyInterval *too* low or it will be tough to
@@ -20,9 +20,10 @@
 #include <Keyboard.h>
 
 int ledState = LOW;           // default LED state (don't change)
-const int ledPin =  13;       // the number of the internal LED pin (usually pin 13)
+const int ledPin =  13;       // internal LED pin (usually pin 13)
+const int ledDelay =  500;    // LED hold delay of X ms
 const int keyWrite = 0x81;    // what keystroke to type (0x81 == Left-Shift)
-const int keyInterval = 4000; // input the keystroke every X ms (don't set this too low!)
+const int keyInterval = 4000; // input the keystroke every X ms
 
 
 void setup() {
@@ -35,6 +36,7 @@ void ledwrite() {
     ledState = HIGH;
   } else {
     ledState = LOW;
+    delay(ledDelay);
   }
   digitalWrite(ledPin, ledState);
 }
